422a92edcbf81bb7060d91066e89dda3ae4f8995
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-01-15 14:36:29 +0900

    feat: themeスイッチコンポーネント

---
+++ components/ThemeSwitch.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+function ThemeSwitch() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  const text = darkMode ? 'Light Mode' : 'Dark Mode'
+  console.log('[ThemeSwitch]', darkMode)
+  return (
+    <>
+      <button onClick={() => setDarkMode(!darkMode)}>{text}</button>
+      <style jsx>
+        {`
+          button {
+            background: none;
+            border: none;
+            cursor: pointer;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default ThemeSwitch
